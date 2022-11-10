523383a53b219de07d90c3389212250786cfe183
Christian Repnik <kris18star@gmail.com>
2022-11-10 08:14:57 +0300

    Add index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Автор: Репник Кристиан
+*
+* Дата реализации: 09.11.2022 21.58
+*
+* Дата изменения: 10.11.2022 8:07
+*
+* Объединение класса User и UserService
+*/
+if ((include 'UserService.php') == FALSE) {
+    exit("<p>Compilation error</p>");
+}
